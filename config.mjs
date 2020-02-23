865216ea14a0b3f9fc51f3cad0f386972997653f
jascha <jascha@jaeh.at>
2020-02-23 21:45:42 +0100

    use @magic-themes/project as base

--- config.mjs
+++ config.mjs
@@ -3,6 +3,6 @@ export default {
   WEB_ROOT: '/magic-theme/',
   PUBLIC: 'docs',
   URL: 'grundstein.github.io/magic-theme',
-  THEME: 'grundstein-magic-theme',
+  THEME: ['project', 'grundstein-magic-theme'],
   HOIST: ['LightSwitch', 'Gdpr'],
 }
